15683df17251e7be396c251144a50105260eca9e
lilyruth <doodlepath@gmail.com>
2022-02-17 18:31:45 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Rate limiting
+const limiter = rateLimit({
+ windowMS: 10 * 60 * 1000, //10 minutes
+ max: 10
+})
+app.use(limiter);
+app.set('trust proxy', 1)
+
+// Routes
+app.use('/api', require('./routes'))
+
+// Enable cors
+app.use(cors());
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+// add '&count=num' to get that particular count
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const needle = require('needle');
+
+// Env variable
+const BASE_URL = process.env.BASE_URL;
+const API_NAME = process.env.API_NAME;
+const API_KEY = process.env.API_KEY;
+
+router.get('/', async (req, res) => {
+
+ try {
+  const apiRes = await needle('get', `${BASE_URL}?${API_NAME}=${API_KEY}&count=5`);
+
+  const data = apiRes.body;
+ 
+  res.status(200).json(data);
+ } 
+ catch (error) {
+  res.status(500).json({error})
+ }
+ 
+})
+
+module.exports = router;
